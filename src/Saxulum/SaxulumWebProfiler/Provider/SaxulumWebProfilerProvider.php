e23274cd224933f1ca9878e924e5eac49532df1b
Dominik Zogg <dominik.zogg@gmail.com>
2013-07-14 20:13:13 +0200

    enhance provider

--- src/Saxulum/SaxulumWebProfiler/Provider/SaxulumWebProfilerProvider.php
+++ src/Saxulum/SaxulumWebProfiler/Provider/SaxulumWebProfilerProvider.php
@@ -20,15 +20,14 @@ class SaxulumWebProfilerProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        if (isset($app['twig'])) {
+        if (isset($app['profiler'])) {
+
             $app['twig'] = $app->share($app->extend('twig', function(\Twig_Environment $twig) {
                 $twig->addExtension(new DoctrineExtension());
 
                 return $twig;
             }));
-        }
 
-        if (isset($app['twig.loader.filesystem'])) {
             $app['twig.loader.filesystem'] = $app->share($app->extend('twig.loader.filesystem',
                 function(\Twig_Loader_Filesystem $twigLoaderFilesystem) {
                     $twigLoaderFilesystem->addPath(dirname(__DIR__). '/Resources/views', 'SaxulumWebProfilerProvider');
@@ -36,9 +35,7 @@ class SaxulumWebProfilerProvider implements ServiceProviderInterface
                     return $twigLoaderFilesystem;
                 }
             ));
-        }
 
-        if (isset($app['data_collectors'])) {
             $dataCollectors = $app['data_collectors'];
             $dataCollectors['db'] = $app->share(function ($app) {
                 $dataCollector = new DoctrineDataCollector($app['doctrine']);
@@ -51,9 +48,7 @@ class SaxulumWebProfilerProvider implements ServiceProviderInterface
                 return $dataCollector;
             });
             $app['data_collectors'] = $dataCollectors;
-        }
 
-        if (isset($app['data_collector.templates'])) {
             $dataCollectorTemplates = $app['data_collector.templates'];
             $dataCollectorTemplates[] = array('db', '@SaxulumWebProfilerProvider/Collector/db.html.twig');
             $app['data_collector.templates'] = $dataCollectorTemplates;

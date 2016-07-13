f0620b978d6c4415c851ed97c28094f9b95ac1da
Dominik Zogg <dominik.zogg@gmail.com>
2016-07-13 20:22:37 +0200

    prepare silex 2.0 version

--- src/Provider/SaxulumWebProfilerProvider.php
+++ src/Provider/SaxulumWebProfilerProvider.php
@@ -27,28 +27,28 @@ class SaxulumWebProfilerProvider implements ServiceProviderInterface
     {
         if (isset($app['profiler'])) {
 
-            $app['twig'] = $app->share($app->extend('twig', function (\Twig_Environment $twig) {
+            $app['twig'] = $app->extend('twig', function (\Twig_Environment $twig) {
                 $twig->addExtension(new DoctrineExtension());
 
                 return $twig;
-            }));
+            });
 
-            $app['twig.loader.filesystem'] = $app->share($app->extend('twig.loader.filesystem',
+            $app['twig.loader.filesystem'] = $app->extend('twig.loader.filesystem',
                 function (\Twig_Loader_Filesystem $twigLoaderFilesystem) {
                     $twigLoaderFilesystem->addPath(dirname(__DIR__). '/Resources/views', 'SaxulumWebProfilerProvider');
 
                     return $twigLoaderFilesystem;
                 }
-            ));
+            );
 
             $app['data_collectors'] = $app->extend('data_collectors',
-                $app->share(function(array $collectors) use ($app) {
+                function(array $collectors) use ($app) {
                     if(isset($app['doctrine'])) {
-                        $app['saxulum.orm.logger'] = function ($app) {
+                        $app['saxulum.orm.logger'] = $app->factory(function ($app) {
                             return new DbalLogger($app['monolog'], $app['stopwatch']);
-                        };
+                        });
 
-                        $collectors['db'] = $app->share(function ($app) {
+                        $collectors['db'] = function ($app) {
                             $dataCollector = new DoctrineDataCollector($app['doctrine']);
                             foreach ($app['doctrine']->getConnectionNames() as $name) {
                                 $logger = $app['saxulum.orm.logger'];
@@ -57,56 +57,56 @@ class SaxulumWebProfilerProvider implements ServiceProviderInterface
                             }
 
                             return $dataCollector;
-                        });
+                        };
                     }
 
                     if(isset($app['doctrine_mongodb'])) {
                         $app['saxulum.webprofiler.mongodb.odm.logger.batchthreshold'] = 4;
 
-                        $app['saxulum.mongodb.odm.logger'] = $app->share(function ($app) {
+                        $app['saxulum.mongodb.odm.logger'] = function ($app) {
                             $logger = new DoctrineMongoDbLogger($app['monolog']);
                             $logger->setBatchInsertThreshold($app['saxulum.mongodb.odm.logger.batchthreshold']);
 
                             return $logger;
-                        });
+                        };
 
-                        $app['saxulum.mongodb.odm.loggers'] = $app->share(function ($app) {
+                        $app['saxulum.mongodb.odm.loggers'] = function ($app) {
                             $loggers = array();
                             $loggers[] = $app['saxulum.mongodb.odm.logger'];
                             $loggers[] = $app['saxulum.mongodb.odm.datacolletor'];
 
                             return $loggers;
-                        });
+                        };
 
-                        $app['saxulum.mongodb.odm.aggregatelogger'] = $app->share(function ($app) {
+                        $app['saxulum.mongodb.odm.aggregatelogger'] = function ($app) {
                             $logger = new DoctrineMongoDbAggregateLogger($app['saxulum.mongodb.odm.loggers']);
 
                             return $logger;
-                        });
+                        };
 
                         $aggregatedLogger = $app['saxulum.mongodb.odm.aggregatelogger'];
                         $app['doctrine_mongodb'] = $app->extend('doctrine_mongodb',
-                            $app->share(function(ManagerRegistry $registry) use($aggregatedLogger) {
+                            function(ManagerRegistry $registry) use($aggregatedLogger) {
                                 foreach ($registry->getConnectionNames() as $name) {
                                     $registry->getConnection($name)->getConfiguration()->setLoggerCallable(array($aggregatedLogger, 'logQuery'));
                                 }
                             }
-                        ));
+                        );
 
-                        $collectors['mongodb'] = $app->share(function ($app) {
+                        $collectors['mongodb'] = function ($app) {
                             $dataCollector = new DoctrineMongoDbDataCollector();
                             $dataCollector->setBatchInsertThreshold($app['saxulum.mongodb.odm.logger.batchthreshold']);
 
                             return $dataCollector;
-                        });
+                        };
                     }
 
                     return $collectors;
                 }
-            ));
+            );
 
             $app['data_collector.templates'] = $app->extend('data_collector.templates',
-                $app->share(function(array $dataCollectorTemplates) use ($app) {
+                function(array $dataCollectorTemplates) use ($app) {
                     if(isset($app['doctrine'])) {
                         $dataCollectorTemplates[] = array('db', '@SaxulumWebProfilerProvider/Collector/db.html.twig');
                     }
@@ -116,7 +116,7 @@ class SaxulumWebProfilerProvider implements ServiceProviderInterface
 
                     return $dataCollectorTemplates;
                 }
-            ));
+            );
         }
     }
 }
